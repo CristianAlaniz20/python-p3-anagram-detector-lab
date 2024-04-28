eac53ec78121df19d5509e2d217b83af85225cce
Cristian Alaniz Benitez <alaniz21cris@gmail.com>
2024-04-27 17:04:58 -0700

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,27 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, list):
+        result = []
+        for word in list:
+            string1 = [letter for letter in self.word]
+            string2 = [letter for letter in word]
+            if sorted(string1) == sorted(string2):
+                result.append(word)
+        return result
+
+
+#compare word with each word in list
+#if any letter in word matches with a word in the list
+# -make them cancel out
+# continue comparing until all letters of word are used OR
+# no letter match
+
+#if letters match, push word in list to new list
+# -continue to next word
+# repeat steps
+
+# return new list
